17e778c3e7b6aad0d5a03f0ba9ec9bb84fdef70c
Dev Keshwani <dev.keshwani345@gmail.com>
2023-09-15 11:25:12 +0530

    added more codes

---
+++ arrays/max-min.c
@@ -0,0 +1,27 @@
+// find smallest and largest number at their positions in array
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    int arr[5], i, max, min, maxpos = 0, minpos = 0;
+    printf("Enter 5 numbers: ");
+    for (i = 0; i < 5; i++)
+        scanf("%d", &arr[i]);
+    max = arr[0];
+    min = arr[0];
+    for (i = 1; i < 5; i++)
+    {
+        if (arr[i] > max)
+        {
+            max = arr[i];
+            maxpos = i + 1;
+        }
+        if (arr[i] < min)
+        {
+            min = arr[i];
+            minpos = i + 1;
+        }
+    }
+    printf("Largest number is %d at position %d", max, maxpos);
+    printf("\nSmallest number is %d at position %d", min, minpos);
+    return 0;
+}
---
+++ arrays/sec-max.c
@@ -0,0 +1,26 @@
+// second largest number in an array
+
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    int arr[5], i, max, max2;
+    printf("Enter 5 numbers: ");
+    for (i = 0; i < 5; i++)
+        scanf("%d", &arr[i]);
+    max = arr[0];
+    max2 = 0;
+    for (i = 1; i < 5; i++)
+    {
+        if (arr[i] > max)
+        {
+            max2 = max;
+            max = arr[i];
+        }
+        else if (arr[i] > max2)
+        {
+            max2 = arr[i];
+        }
+    }
+    printf("Second largest number is %d", max2);
+    return 0;
+}
