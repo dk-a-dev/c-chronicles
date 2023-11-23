4de05d80b7e9075f60b8a4ccf882fc6db3c6b14d
Dev Keshwani <dev.keshwani345@gmail.com>
2023-11-23 21:29:02 +0530

    refactor: optimised sorting algo

--- sorting/bubble-sort.c
+++ sorting/bubble-sort.c
@@ -9,9 +9,9 @@ int main(int argc, char const *argv[])
     {
         scanf("%d", &arr[i]);
     }
-    for (int i = n - 1; i > 0; i--)
+    for (int i = 0; i < n-1; i++)
     {
-        for (int j = 0; j < i; j++)
+        for (int j = 0; j < n-1-i; j++)
         {
             if (arr[j] > arr[j + 1])
             {
