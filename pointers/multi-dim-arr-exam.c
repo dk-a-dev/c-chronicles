4d23bad076d59d826216709969f0588443d7f8b5
Dev Keshwani <dev.keshwani345@gmail.com>
2023-12-08 12:19:13 +0530

    refactor: improved terminal output

--- pointers/multi-dim-arr-exam.c
+++ pointers/multi-dim-arr-exam.c
@@ -10,9 +10,8 @@ int main()
     for (i = 0; i < 3; i++)
     {
         printf("Address of %dth array = %d %d\n", i, arr[i], *(arr + i));
-
         for (j = 0; j < 4; j++)
-            printf("%d %d ", arr[i][j], *(*(arr + i) + j));
+            printf("Ele:%d Address:%d ", arr[i][j], (*(arr + i) + j));
         printf("\n");
     }
 
